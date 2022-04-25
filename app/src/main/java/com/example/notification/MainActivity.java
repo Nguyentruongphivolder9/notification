954ce78de24b3ec04c6633a7173f9fe9fc44bcdf
phi <nguyentruongphi15032003@gmail.com>
2022-04-25 18:47:33 +0700

    finish

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -1,56 +1,19 @@
 package com.example.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mBtnOpen, mBtnClose;
-    String CHANNEL_ID = "My_channel";
+    Button mBtnNavigateScreen2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBtnOpen = findViewById(R.id.buttonOpenNotification);
-        mBtnClose = findViewById(R.id.buttonCloseNotification);
-
-        mBtnOpen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this,MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,0);
-
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,CHANNEL_ID);
-                builder.setContentTitle("Thông báo");
-                builder.setContentText("Bạn còn 5 voucher sắp hết hạn. Hãy vào kiểm tra!!");
-                builder.setShowWhen(true);
-                builder.setSmallIcon(android.R.drawable.btn_star);
-                builder.setVibrate(new long[]{1000,200,2000,200});
-                builder.setContentIntent(pendingIntent);
-
-                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                    NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "Admod", NotificationManager.IMPORTANCE_DEFAULT);
-                    notificationChannel.setImportance(NotificationManager.IMPORTANCE_LOW);
-                    notificationManager.createNotificationChannel(notificationChannel);
-                }
-
-                notificationManager.notify(1,builder.build());
-            }
-        });
+        mBtnNavigateScreen2 = findViewById(R.id.buttonNavigateScreen2);
     }
 }
